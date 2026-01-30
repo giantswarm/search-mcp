68ceaf41c264922bb104cb1ae669dc722c2b389d
taylorbot <dev@giantswarm.io>
2026-01-30 11:28:17 +0000

    Bump version to 0.2.1-dev

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "search-mcp"
 	source      = "https://github.com/giantswarm/search-mcp"
-	version     = "0.2.0"
+	version     = "0.2.1-dev"
 )
 
 func Description() string {
